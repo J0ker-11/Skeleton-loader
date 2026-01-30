0edd2b81ae7da3174aead86e0848d18fac878d44
J0ker11 <primetech444@gmail.com>
2026-01-30 21:02:46 +0100

    fix: fixed the content width

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -11,8 +11,8 @@ const SkeletonCard = () => {
       <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
 
       {/* Description placeholder */}
-      <div className="h-3 bg-gray-300 rounded w-full mb-1"></div>
-      <div className="h-3 bg-gray-300 rounded w-5/6"></div>
+      <div className="h-4 bg-gray-300 rounded w-full mb-1"></div>
+      <div className="h-4 bg-gray-300 rounded w-5/6"></div>
 
     </div>
   )
