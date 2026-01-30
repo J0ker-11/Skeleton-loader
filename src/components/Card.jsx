fb2c32cc52e28c5651d656e6c1374fe0dd985481
J0ker11 <primetech444@gmail.com>
2026-01-30 20:40:45 +0100

    fix: fixed the content width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Card = ({title,image,description}) => {
   return (
-    <div className="w-full max-w-[600px] mx-auto p-4 border rounded-lg">
+    <div className="w-full max-w-[400px] mx-auto p-2 border rounded-lg">
       
       <img
         src={image}
         alt={title}
-        className="h-40 w-full object-cover rounded mb-4"
+        className="h-full w-full object-cover rounded mb-4"
       />
 
       <h3 className="font-semibold mb-2">{title}</h3>
